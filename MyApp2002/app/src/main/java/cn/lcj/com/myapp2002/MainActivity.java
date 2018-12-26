158a20ec83568bc06bfef4f8161ea7c6055f2890
e <Lcj@163.com>
2018-12-26 19:52:17 +0800

    Revert "第一次修改"
    This reverts commit c33e71ff3790d9acd3136818f164b1ac6ca07a0b.

--- MyApp2002/app/src/main/java/cn/lcj/com/myapp2002/MainActivity.java
+++ MyApp2002/app/src/main/java/cn/lcj/com/myapp2002/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("1111");
-        System.out.println("2222");
 
     }
 }
